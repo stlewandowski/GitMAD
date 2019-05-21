6831e59594a17de1c95574f09f8f5c164061fd79
deepdivesec <45269493+deepdivesec@users.noreply.github.com>
2019-05-21 16:43:08 -0700

    Update github_search_db.sql

--- github_search_db.sql
+++ github_search_db.sql
@@ -4,6 +4,7 @@
 -- MySQL Workbench Forward Engineering
 
 -- Script to input into existing MySQL 8 database.
+-- This will create the two tables that GitMAD will use.
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
